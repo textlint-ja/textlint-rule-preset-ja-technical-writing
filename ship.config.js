cfdf75bf061e53806855c09aabaca37d2e2370c2
azu <azuciao@gmail.com>
2021-07-01 23:54:52 +0900

    feat(release): use changesets instead of shipjs

--- ship.config.js
+++
@@ -1,8 +0,0 @@
-const semver = require("semver");
-module.exports = {
-    buildCommand: () => null,
-    getNextVersion: ({ revisionRange, commitTitles, commitBodies, currentVersion, dir }) => {
-        // always major update
-        return semver.inc(currentVersion, "major");
-    }
-};
